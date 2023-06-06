9d462f22b6d99d7aca045e88f36b73b2d0701777
anthozb3x <anthony.beal@clsi.fr>
2023-06-06 14:56:34 +0200

    ajout du cvg calendat

--- vue/v_emprunt.php
+++ vue/v_emprunt.php
@@ -1,4 +1,4 @@
-<h2>mes Emprunts:</h2>
+<h2>Mes Emprunts:</h2>
 <hr>
 <div class="container-fluid">
     <?php
@@ -17,11 +17,17 @@
                             <h4 class="card-title"><?= $unEnprunts->getDocument()->getTitre() ?></h4>
                             <hr>
                             <div class="row">
+                                <div class="col-auto">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-calendar2" viewBox="0 0 16 16">
+                                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
+                                        <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z" />
+                                    </svg>
+                                </div>
                                 <div class="col">
                                     <div class="card-text">
-                                        <span class="date mr-3">2023-05-08</span>
+                                        <span class="date mr-3"><?= $unEnprunts->getDateDebut() ?></span>
                                         <span class="arrow">→</span>
-                                        <span class="date ml-3">2023-06-29</span>
+                                        <span class="date ml-3"><?= $unEnprunts->getDateFin() ?></span>
                                     </div>
                                     <div class="card-text">
                                         <span class="indication mr-3">Date de l'emprunt</span>
